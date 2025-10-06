8978d82e76d408932020d260736e1728054a70a2
Swaroop K R <141357728+swar41@users.noreply.github.com>
2025-10-06 17:27:02 +0530

    Delete Backend_v4 directory

--- Backend_v4/DTOs/DepartmentDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Project_Version1.DTOs
-{
-    public class DepartmentDto
-    {
-        public int DeptId { get; set; }
-        public string DeptName { get; set; } = null!;
-    }
-}
--- Backend_v4/DTOs/TagDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project_Version1.DTOs
-{
-    public class TagDto
-    {
-        public int TagId { get; set; }
-        public string TagName { get; set; } = null!;
-        public int DeptId { get; set; }
-    }
-}
